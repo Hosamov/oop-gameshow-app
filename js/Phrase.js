c85a82ef013abed0def19729391322ca655b9f04
Hosamov <hosamov@hotmail.com>
2021-01-14 04:31:19 -0800

    updated game files

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  //constructor holds actual phrase object is representing
+  constructor(phrase) {
+    this.phrase = phrase;
+  }
+
+  //Add letter placeholders to the display when the game starts
+  addPhraseToDisplay() {
+
+  }
+
+  //Check to see if the letter selected by the player matches a letter in the phrase
+  checkLetter() {
+
+  }
+
+  //Reveal letter(s) on the board that matches the player's selection
+  showMatchedLetter(){
+    //1. Select all of the letter DOM elements that have a CSS class name that matches the selected letter
+    //2. Replace each selected element's 'hide' CSS class with the 'show' CSS class
+  }
+}
